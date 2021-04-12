f487e9971eca3499671225aaa82020d9dd7ca332
Quang Truong <truongdangquang1998@gmail.com>
2021-04-12 07:02:24 +0700

    update

---
+++ EnglishExamPreparation/Entities/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Context :DbContext
+    {
+        public Context():base("name=ConnectionStringSql")
+        {
+
+        }
+    }
+}
