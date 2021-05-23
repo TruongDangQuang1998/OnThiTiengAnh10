7894bb8ae22af91fbc5861739c93d44fcb1ccf62
Quang Truong <truongdangquang1998@gmail.com>
2021-05-24 00:44:20 +0700

    update db and api

---
+++ EnglishExamPreparation/EnglishV2/Models/MultipleChoiceQuestionModel.cs
@@ -0,0 +1,93 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnglishV2.Models
+{
+    #region Model
+    public class MultipleChoiceQuestionModel
+    {
+        public int Id { get; set; }
+        public string QuestionContent { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public int Answer { get; set; }
+        public int TypeQuestionId { get; set; }
+        public string TypeQuesitonName { get; set; }
+        public bool IsDelete { get; set; }
+    }
+    public class MultipleChoiceQuestionListModel : ApiJsonResult
+    {
+        public MultipleChoiceQuestionListModel()
+        {
+             MultipleChoiceQuestionModels = new List<MultipleChoiceQuestionModel>();
+            Total = 0;
+        }
+        public List<MultipleChoiceQuestionModel>  MultipleChoiceQuestionModels { get; set; }
+        public int Total { get; set; }
+    }
+    public class MultipleChoiceQuestionDetailModel : ApiJsonResult
+    {
+        public int Id { get; set; }
+        public string QuestionContent { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public int Answer { get; set; }
+        public int ExamId { get; set; }
+        public string ExamName { get; set; }
+        public int TypeQuestionId { get; set; }
+        public string TypeQuestionName { get; set; }
+        public bool IsDelete { get; set; }
+    }
+    public class MultipleChoiceQuestionEditModel
+    {
+        public int Id { get; set; }
+        public string QuestionContent { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public int Answer { get; set; }
+        public int ExamId { get; set; }
+        //public string ExamName { get; set; }
+        public int TypeQuestionId { get; set; }
+        //public string TypeQuestionName { get; set; }
+        public bool IsDelete { get; set; }
+    }
+
+    public class MultipleChoiceQuestionAddModel
+    {
+        public int Id { get; set; }
+        public string QuestionContent { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public int Answer { get; set; }
+        public int ExamId { get; set; }
+        //public string ExamName { get; set; }
+        public int TypeQuestionId { get; set; }
+        //public string TypeQuestionName { get; set; }
+        public bool IsDelete { get; set; }
+    }
+    #endregion
+
+    #region 
+    public class MultipleChoiceQuestionValidator : AbstractValidator<MultipleChoiceQuestionEditModel>
+    {
+        /// <summary>
+        /// Define validators here
+        /// </summary>
+        public MultipleChoiceQuestionValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+        }
+    }
+    #endregion
+}
---
+++ EnglishExamPreparation/EnglishV2/Models/UserRoleModel.cs
@@ -0,0 +1,76 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnglishV2.Models
+{
+    //public class UserRoleModel
+    //{
+    //}
+    #region Model
+    public class UserRoleModel
+    {
+        public UserRoleModel()
+        {
+            UserModels = new List<UserModel>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDelete { get; set; }
+        public List<UserModel> UserModels { get; set; }
+    }
+    public class UserRoleListModel : ApiJsonResult
+    {
+        public UserRoleListModel()
+        {
+            UserRoleList = new List<UserRoleModel>();
+            Total = 0;
+        }
+        public List<UserRoleModel> UserRoleList { get; set; }
+        public int Total { get; set; }
+    }
+    public class UserRoleDetailModel : ApiJsonResult
+    {
+        public UserRoleDetailModel()
+        {
+            UserModels = new List<UserModel>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDelete { get; set; }
+        public List<UserModel> UserModels { get; set; }
+    }
+    public class UserRoleEditModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDelete { get; set; }
+    }
+
+    public class UserRoleAddModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDelete { get; set; }
+    }
+    #endregion
+
+    #region 
+    public class UserRoleValidator : AbstractValidator<UserRoleEditModel>
+    {
+        /// <summary>
+        /// Define validators here
+        /// </summary>
+        public UserRoleValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+        }
+    }
+    #endregion
+}
